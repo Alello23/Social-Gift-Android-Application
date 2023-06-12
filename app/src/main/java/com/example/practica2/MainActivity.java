d7af3c9f90e8c383c54881c54e6100f9371bcb6d
Alello23 <alexliu.762003@gmail.com>
2023-06-12 16:50:22 +0200

    test

--- app/src/main/java/com/example/practica2/MainActivity.java
+++ app/src/main/java/com/example/practica2/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -20,35 +22,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.textView);
-        // ...
+        textView = (TextView) findViewById(R.id.textView);
 
-        // Instantiate the RequestQueue.
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               makeRequest();
+            }
+        });
+    }
+
+    private void makeRequest() {
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://balandrau.salle.url.edu/i3/mercadoexpress/api/v1/products";
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
                     @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response.substring(0,500));
+                    public void onResponse(JSONObject response) {
+                        Log.e("resposta", "La resposta es: "+ response.toString());
+                        textView.setText(response.toString());
                     }
                 }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
-            }
-        });
 
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("resposta", "Hi ha hagut un error:" + error);
+                        textView.setText("Hi ha hagut un error:" + error);
+                    }
+                }
+                );
+
+        queue.add(jsonObjectRequest);
     }
 }
