dbdf8cb932669ccbe0769e22b1784afaf97b9b33
Alello23 <alexliu.762003@gmail.com>
2023-06-13 14:55:38 +0200

    temporal model

--- app/src/main/java/com/example/practica2/MainActivity.java
+++ app/src/main/java/com/example/practica2/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.practica2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,44 +25,60 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private TextView textView;
+    private Button SignIn;
+    private TextView ChangeBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//               makeRequest();
-//            }
-//        });
+        SignIn = (Button) findViewById(R.id.SI_SignIn_Button);
+
+        ChangeBox = (TextView) findViewById(R.id.SI_SignUpLabel);
+        ChangeBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Acción a realizar cuando se hace clic en el texto
+                // Puedes colocar aquí el código que deseas ejecutar al hacer clic
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//               makeRequest();             //Canbiar a la logica de codigo
+            }
+        });
+
+
     }
 
-    private void makeRequest() {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://balandrau.salle.url.edu/i3/mercadoexpress/api/v1/products";
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.e("resposta", "La resposta es: "+ response.toString());
-                        textView.setText(response.toString());
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("resposta", "Hi ha hagut un error:" + error);
-                        textView.setText("Hi ha hagut un error:" + error);
-                    }
-                }
-                );
-
-        queue.add(jsonObjectRequest);
-    }
+//    private void makeRequest() {
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//        String url ="https://balandrau.salle.url.edu/i3/mercadoexpress/api/v1/products";
+//
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+//                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+//
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        Log.e("resposta", "La resposta es: "+ response.toString());
+//                        textView.setText(response.toString());
+//                    }
+//                }, new Response.ErrorListener() {
+//
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.e("resposta", "Hi ha hagut un error:" + error);
+//                        textView.setText("Hi ha hagut un error:" + error);
+//                    }
+//                }
+//                );
+//
+//        queue.add(jsonObjectRequest);
+//    }
 }
